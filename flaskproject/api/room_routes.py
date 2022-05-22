1fc9aafa743f8212321db139565b2faf29959090
Downster <b.downing.code@gmail.com>
2022-05-22 13:38:14 -0700

    Getting ready for socket.io

--- flaskproject/api/room_routes.py
+++ flaskproject/api/room_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from flask_login import current_user
 from ..models import db, Room, Chat, User
+from .auth_routes import token_required
 
 
 def validation_errors_to_error_messages(validation_errors):
@@ -16,13 +17,15 @@ def validation_errors_to_error_messages(validation_errors):
 room_routes = Blueprint('rooms', __name__)
 
 @room_routes.route('/<int:roomId>')
-def get_room(roomId):
+@token_required
+def get_room(current_user, roomId):
     room = Room.query.get(roomId)
     return room.to_dict()
 
 
 @room_routes.route('/', methods=['POST'])
-def create_room():
+@token_required
+def create_room(current_user):
     form = RoomForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
@@ -34,7 +37,8 @@ def create_room():
 
 
 @room_routes.route('/<int:roomId>', methods=['PATCH'])
-def edit_room(roomId):
+@token_required
+def edit_room(current_user, roomId):
     form = RoomForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
@@ -46,8 +50,9 @@ def edit_room(roomId):
 
 
 @room_routes.route('/<int:roomId>', methods=['DELETE'])
-def delete_room(roomId):
-    room = Room.query.get(int(roomId))
+@token_required
+def delete_room(current_user, roomId):
+    room = Room.query.get(roomId)
     data = room.to_dict()
     db.session.delete(room)
     db.session.commit()
@@ -55,14 +60,16 @@ def delete_room(roomId):
 
 
 @room_routes.route('/<int:roomId>/chats')
-def get_chats(roomId):
+@token_required
+def get_chats(current_user, roomId):
     chats = Chat.query.filter(Chat.room_id == roomId).all()
     return { 'chats': [chat.to_dict() for chat in chats] }
 
 
 @room_routes.route('/<int:roomId>/join', methods=['PATCH'])
-def join_chatroom(roomId):
-    room = Room.query.get(int(roomId))
+@token_required
+def join_chatroom(current_user, roomId):
+    room = Room.query.get(roomId)
     user = User.query.get(current_user.get_id())
     room.active_users.append(user)
     db.session.commit()
@@ -70,8 +77,9 @@ def join_chatroom(roomId):
 
 
 @room_routes.route('/<int:roomId>/leave', methods=['PATCH'])
-def leave_chatroom(roomId):
-    room = Room.query.get(int(roomId))
+@token_required
+def leave_chatroom(current_user, roomId):
+    room = Room.query.get(roomId)
     user = User.query.get(current_user.get_id())
     room.active_users.pop(room.active_users.index(user))
     db.session.commit()
