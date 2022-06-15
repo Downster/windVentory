512dee8141867261969fda878163ea7ceeef7caa
Downster <b.downing.code@gmail.com>
2022-06-15 09:49:23 -0700

    Developer bar added

---
+++ frontend/src/components/SplashPage/DeveloperComponent/index.js
@@ -0,0 +1,34 @@
+import './developer.css'
+
+
+const DeveloperComponent = () => {
+    return (
+        <div className='developer'>
+            <div className='left-dev-div'>
+
+                <a className='windVentory' target="_blank"
+                    href='https://github.com/Downster/SoundClout'>
+                    © 2022 | windVentory
+                </a>
+            </div>
+            <div className='right-dev-div'>
+
+                <p className='developed-by'>  Developed by Brendan Downing </p>
+                <span>
+                    <a target="_blank"
+                        href='https://github.com/Downster'>
+                        <i className='fab fa-github' />
+                    </a>
+                </span>
+                <span>
+                    <a target="_blank"
+                        href='https://www.linkedin.com/in/brendan-downing-641672228/'>
+                        <i className='fab fa-linkedin' />
+                    </a>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default DeveloperComponent
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,6 +3,7 @@ import LoginForm from './LoginForm'
 import { useLocation } from "react-router"
 import { useState } from "react"
 import './SplashPage.css'
+import DeveloperComponent from "./DeveloperComponent"
 
 
 const SplashPage = () => {
@@ -10,10 +11,17 @@ const SplashPage = () => {
     const [signup, setSignup] = useState(false);
 
     return (
-        <div className="splash-page-container">
-            <h1 className="intro-header">windVentory</h1>
-            {signup ? <SignupForm setSignup={setSignup} /> : <LoginForm setSignup={setSignup} />}
-        </div>
+        <>
+            <div className="splash-page-wrapper">
+                <div className="splash-page-container">
+                    <h1 className="intro-header">windVentory</h1>
+                    {signup ? <SignupForm setSignup={setSignup} /> : <LoginForm setSignup={setSignup} />}
+                </div>
+                <div className="developer-div">
+                    <DeveloperComponent />
+                </div>
+            </div>
+        </>
     )
 }
 
