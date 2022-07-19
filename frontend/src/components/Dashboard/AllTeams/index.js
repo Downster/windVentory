e954cb3c2c736e6836edc0d863476568937dcb88
Downster <b.downing.code@gmail.com>
2022-07-19 09:48:29 -0700

    More styling

--- frontend/src/components/Dashboard/AllTeams/index.js
+++ frontend/src/components/Dashboard/AllTeams/index.js
@@ -17,10 +17,9 @@ const AllTeams = () => {
 
     return (
         <>
-            <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                {(teamsObject.length) ? teamsObject.map((team) => (
-                    <TeamCard key={team.id} team={team} admin={(user.role.includes('Admin')) ? true : false} />)) : <h1>There are no teams, create one?</h1>}
-            </ul>
+
+            {(teamsObject.length) ? teamsObject.map((team) => (
+                <TeamCard key={team.id} team={team} admin={(user.role.includes('Admin')) ? true : false} />)) : <h1>There are no teams, create one?</h1>}
             <CreateTeamModal />
         </>
     )
